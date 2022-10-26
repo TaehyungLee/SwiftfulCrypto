9b636c8b276eca1ff6bd970ee6b2a9afd36e71f5
LeeTaehyung <thlee@kb-sys.com>
2022-10-26 18:05:18 +0900

    Initial Commit

---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by Taehyung Lee on 2022/10/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
